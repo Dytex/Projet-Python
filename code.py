b298a6d98d9c8153cb25bb65d6be2a9ac44fa351
Dytex <58592137+Dytex@users.noreply.github.com>
2020-04-03 15:46:46 +0200

    Delete code.py

--- code.py
+++
@@ -1,8 +0,0 @@
-from tkinter import *
-
-fenetre = Tk()
-
-canvas = Canvas(fenetre, width=150, height=120, background='yellow')
-txt = canvas.create_text(75, 60, text="Start", font="Arial 16 italic", fill="blue")
-canvas.pack()
-fenetre.mainloop()
